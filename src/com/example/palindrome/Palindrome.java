b50d175b3c3da72be3a7d1a75f93eeb9cf0de29c
Kyle Ofori <kyleofori@Administrators-MacBook-Air-3.local>
2014-09-15 09:46:17 -0400

    Adding more details in palindrome

--- src/com/example/palindrome/Palindrome.java
+++ src/com/example/palindrome/Palindrome.java
@@ -1,10 +1,25 @@
 package com.example.palindrome;
 
+import java.util.Scanner;
+
 /**
  * Created by kyleofori on 9/15/14.
  */
 public class Palindrome {
     public static void main(String[] args) {
-        
+        Scanner palinReader = new Scanner(System.in);
+        String palindrome;
+
+        System.out.println("Enter the word that you want to check for palindrome-ness.");
+
+        if (palinReader.hasNext())
+
+        //count # letters in the word, save that as some integer "i"
+        //check if i is the same character as 0
+        //check if i-1 is the same character as 1
+        //and so on...
+        //for (int i = (number of letters) ; i>=0; i--)
+
+        //is the word the same as the reversed word?
     }
 }
